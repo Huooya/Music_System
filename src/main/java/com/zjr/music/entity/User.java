b214f2deea4f68937bfd076db36865ff46340f9f
Huooya <61089887+Huooya@users.noreply.github.com>
2023-03-11 10:47:13 +0800

    完善用户字段

--- src/main/java/com/zjr/music/entity/User.java
+++ src/main/java/com/zjr/music/entity/User.java
@@ -3,6 +3,13 @@ package com.zjr.music.entity;
 public class User {
     private String username;
     private String password;
+    private String sex;
+    private String introduction;
+    private String area;
+    private Integer dynamic_state;
+    private Integer attention;
+    private Integer fan;
+    private String avatar;
 
     public String getUsername() {
         return username;
@@ -18,5 +25,61 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public Integer getDynamic_state() {
+        return dynamic_state;
+    }
+
+    public void setDynamic_state(Integer dynamic_state) {
+        this.dynamic_state = dynamic_state;
+    }
+
+    public Integer getAttention() {
+        return attention;
+    }
+
+    public void setAttention(Integer attention) {
+        this.attention = attention;
+    }
+
+    public Integer getFan() {
+        return fan;
+    }
+
+    public void setFan(Integer fan) {
+        this.fan = fan;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
     }
 }
