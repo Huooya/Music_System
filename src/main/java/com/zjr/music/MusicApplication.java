9fa750896fc7ee5478a0c3d399f49267a25b03e5
Huooya <61089887+Huooya@users.noreply.github.com>
2023-03-13 17:24:53 +0800

    修改了一百个坑，成功引入nacos

--- src/main/java/com/zjr/music/MusicApplication.java
+++ src/main/java/com/zjr/music/MusicApplication.java
@@ -2,8 +2,10 @@ package com.zjr.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MusicApplication {
 
 	public static void main(String[] args) {
